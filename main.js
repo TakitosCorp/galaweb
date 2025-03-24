a4aef90941bb6b070e37f2c02b28bcca903d24c6
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-03-24 14:40:26 +0100

    feat: :construction: New page added for YouTube Feed

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { config as configDotenv } from "dotenv"; 
+
+configDotenv();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,6 +18,10 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/yt", (req, res) => {
+  res.render("youtube", { cid: process.env.YOUTUBE_CHANNEL_ID });
 });
 
 app.get("/sw.js", (req, res) => {
